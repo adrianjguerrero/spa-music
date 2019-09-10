22ea0d66d63969b21fa027f1469c55b3ece6bc16
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-10 11:57:11 -0400

    semicolon eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-new':0
+    'no-new':0,
+    'semi': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
