ec9bc6716298143969437539b466435ad28bba86
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-11 19:13:16 -0400

    some styles and router links

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -7,8 +7,10 @@
               <strong>App2Learn</strong>
             </div>
           </div>
-          <div class="navbar-menu">
-            <div class="nav-menu">
+          <div class="navbar-end">
+            <div class="navbar-menu">
+              <router-link class="nav-item nav-link" to="/">Buscar</router-link>
+              <router-link class="nav-item nav-link" to="/about">Nosotros</router-link>
             </div>
           </div>
       </header>
@@ -31,3 +33,22 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.hero header.navbar{
+  padding-top: 1.5em;
+  padding-left: .5em;
+
+  .navbar-end{
+    .navbar-menu{
+      
+      .nav-link{
+        margin-right: 1em;
+        color: white;
+      }
+
+    }
+    justify-content: space-around;
+  }
+}
+</style>
