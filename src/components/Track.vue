0113e144029ada5b19d05779da977c7f7f873573
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-11 17:23:33 -0400

    functionality to the track component

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -43,6 +43,10 @@ export default {
   methods: {
     selectedTrack () {
       this.$emit('select', this.track.id);
+      this.$bus.$emit('set-track', this.track);
+      // asi, ya que bus es una instanciacion de Vue,
+      // puede usar los mismos eventos, por lo tanto
+      // cualquiera q lo este escuchando, recibira estas cosas
     }
   }
 }
