c65fc11998fa030184b9ce461c3940e082dceacb
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-11 18:59:27 -0400

    added about route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import Search from './components/Search.vue';
+import About from './components/About.vue';
 
-const routes = [{
-  path: '/',
-  component: Search,
-  name: 'search'
-}]
+const routes = [
+  { path: '/', component: Search, name: 'search' },
+  { path: '/about', component: About, name: 'about' }
+]
 
 export default routes
