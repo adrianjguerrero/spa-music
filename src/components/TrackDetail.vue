b73347d9730c62fcca39c69c118c9267e64d4c9f
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-11 20:04:11 -0400

    loader

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="containers">
+    <cc-loader v-show="isLoading"></cc-loader>
     <div class="colums">
         <div class="column is-4 is-offset-4">
             <cc-track :track="track"></cc-track>
@@ -11,19 +12,26 @@
 <script>
 import trackService from './../services/track';
 import CcTrack from './Track.vue'
+import CcLoader from './shared/Loader.vue'
 export default {
     components: {
-        CcTrack
+        CcTrack,
+        CcLoader
     },
     data () {
         return {
-            track: {}
+            track: {},
+            isLoading: true
         }
     },
     created () {
         const id = this.$route.params.id;
+        this.isLoading = true;
         trackService.getById(id)
-         .then(res => this.track = res)
+         .then(res => {
+             this.track = res
+             this.isLoading = false;
+            });
     }
 
 }
