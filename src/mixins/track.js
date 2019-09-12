f9696034f1d2d26e402c6f9c0911232b93df035b
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-11 22:55:29 -0400

    created mixin

---
+++ src/mixins/track.js
@@ -0,0 +1,9 @@
+const trackMixin = {
+
+}
+
+// los mixin se usan para evitar repetir codigo
+// o sea, si varios componentes usan mismos metodos,
+// aqui podemos definirlos
+
+export default trackMixin;
