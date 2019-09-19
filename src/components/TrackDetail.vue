b32ea43cc10d08371d51986ffe0930285da766e7
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-19 09:08:23 -0400

    using getter on component

--- src/components/TrackDetail.vue
+++ src/components/TrackDetail.vue
@@ -1,51 +1,51 @@
 <template>
-<div class="containers">
-    <cc-loader v-show="isLoading"></cc-loader>
-    <div class="columns" v-if="track && track.album">
-        <div class="column is-3 has-text-centered">
-          <figure class="media-left">
-            <p class="image">
-              <img :src="track.album.images[0].url" alt="album image">
-            </p>
-            <p>
-              <button class="button is-primary is-large">
-                <span class="icon" @click="selectedTrack">&#9658;</span>
-              </button>
-            </p>
-          </figure>
-        </div>
-        <div class="column is-8">
-          <div class="panel">
-            <div class="panel-heading">
-              <h1 class="title">{{track.name}}</h1>
-            </div>
-            <div class="panel-block">
-              <article class="media">
-                <div class="media-cotent">
-                  <div class="content">
-                    <h2>Artistas:</h2>
-                    <ul>
-                      <li v-for="(value, index) in track.artists" :key="index">
-                        <strong>{{index + 1}}: <span>{{value.name}}</span></strong>
-                      </li>
-                    </ul>
-                  </div>
-                  <nav class="level">
-                    <div class="level-left">
-                      <a href="" class="level-item"></a>
+  <div class="containers">
+      <cc-loader v-show="isLoading"></cc-loader>
+      <div class="columns" v-if="track && track.album">
+          <div class="column is-3 has-text-centered">
+            <figure class="media-left">
+              <p class="image">
+                <img :src="track.album.images[0].url" alt="album image">
+              </p>
+              <p>
+                <button class="button is-primary is-large">
+                  <span class="icon" @click="selectedTrack">&#9658;</span>
+                </button>
+              </p>
+            </figure>
+          </div>
+          <div class="column is-8">
+            <div class="panel">
+              <div class="panel-heading">
+                <h1 class="title">{{trackTitle}}</h1>
+              </div>
+              <div class="panel-block">
+                <article class="media">
+                  <div class="media-cotent">
+                    <div class="content">
+                      <h2>Artistas:</h2>
+                      <ul>
+                        <li v-for="(value, index) in track.artists" :key="index">
+                          <strong>{{index + 1}}: <span>{{value.name}}</span></strong>
+                        </li>
+                      </ul>
                     </div>
-                  </nav>
-                </div>
-              </article>
+                    <nav class="level">
+                      <div class="level-left">
+                        <a href="" class="level-item"></a>
+                      </div>
+                    </nav>
+                  </div>
+                </article>
+              </div>
             </div>
           </div>
-        </div>
-    </div>
-</div>
+      </div>
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions} from 'vuex';
+import { mapState, mapActions, mapGetters} from 'vuex';
 import trackMixin from '@/mixins/track';
 import CcLoader from '@/components/shared/Loader.vue'
 export default {
@@ -59,12 +59,13 @@ export default {
     }
   },
   computed: {
-    ...mapState(['track'])
+    ...mapState(['track']),
     /*
      * gracias a esto, creamos una copia de lo que ya hay en nuestro state
      * de forma q si ya tenemos la cancion, no vamos a tener q hacer una peticion
      * para conseguirla
      */
+    ...mapGetters(['trackTitle'])
   },
   methods: {
     ...mapActions(['getTrackById'])
