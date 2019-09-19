350afa30335f61de119441f2746163eef85e09ef
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-09-19 08:37:30 -0400

    action to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import trackService from '@/servcies/track'
 
 Vue.use(Vuex);
 
@@ -12,12 +13,30 @@ const store = new Vuex.Store({
     setTrack (state, track) {
       state.track = track
     }
+  },
+  actions: {
+    getTrackById (context, payload) {
+      return trackService.getById(payload.id)
+      .then(rest => {
+        context.commit('setTrack', res);
+        // context representa el store, entonces aqui estamos haciendo llamado
+        // a la function (mutation) setTrack
+        // se 'comitean' porq asi se lleva un rastro
+        return res;
+      });
+    }
+
   }
 });
 
 /* este archivo es para usar vuex
  * state, propiedades que usaremos
  * mutations, cosas q manupularan el estado
+ * actions, para manejar codigo asincrono
+ * puesto q uhhh vuex no suele esperar
+ * por las mutations al renderizar, si manejan
+ * la data, si se hace una peticion se renderizara
+ * y no esperara la peticion, pero en los actions si
  */
 
 export default store;
